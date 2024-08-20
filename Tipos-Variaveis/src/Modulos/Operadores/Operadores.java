72c181253247d88b1d1d5ee894ea5e284f66dbdd
Murilo Assis <Muassis38@gmail.com>
2024-08-20 12:41:49 -0300

    final da aula de operadores, so registrei os ternarios, pois era o que tinha de duvida

---
+++ Tipos-Variaveis/src/Modulos/Operadores/Operadores.java
@@ -0,0 +1,17 @@
+package Modulos.Operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ?"Verdadeiro":"falso";
+        System.out.println(resultado);
+
+        b = 5;
+
+        String resultado2 = a==b ?"Verdadeiro":"falso";
+        System.out.println(resultado2);
+    }
+}
