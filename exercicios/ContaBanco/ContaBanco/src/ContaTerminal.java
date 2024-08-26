bbdb6b987d3d5f6fdf9640bdb312ff4ac7b1958e
Murilo Assis <Muassis38@gmail.com>
2024-08-26 12:47:21 -0300

    Meu projeto conta banco

---
+++ exercicios/ContaBanco/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,8 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        int numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "MURILO ASSIS";
+        double saldo = 237.48;
+    }
+}
